67b86081bf29d78a8cbcff9f3526c2626f6ccb47
aclinnovator <aclscientist@gmail.com>
2014-06-24 12:33:06 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -18,7 +18,7 @@
 - (void)didLoadFromCCB {
     NSLog(@"Loaded");
     // tell this scene to accept touches
-    self.userInteractionEnabled = TRUE;
+    self.userInteractionEnabled = true;
     NSLog(@"%hhd",self.userInteractionEnabled);
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
