322f545be88e6492d7bc0bc3be3ef5e4f6ac78ba
aclinnovator <aclscientist@gmail.com>
2014-06-24 21:58:35 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.h
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -14,4 +14,6 @@
 
 - (void)didLoadFromCCB;
 - (void)launchPenguin ;
+-(void)releaseCatapult;
+
 @end
--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -16,6 +16,8 @@
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
+    CCNode *_currentPenguin;
+    CCPhysicsJoint *_penguinCatapultJoint;
 
 }
 - (void)reset {
@@ -40,9 +42,40 @@
     CGPoint touchLocation = [touch locationInNode:_contentNode];
     if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation)){
         _mouseJointNode.position = touchLocation;
-         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f]; 
+         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+        _currentPenguin = [CCBReader load:@"Penguin"];
+        CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
+        _currentPenguin.position = [_physicsNode convertToNodeSpace:penguinPosition];
+        [_physicsNode addChild:_currentPenguin];
+        _currentPenguin.physicsBody.allowsRotation = FALSE;
+        _penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
+        
+        
     }
     
+}
+
+-(void)releaseCatapult {
+    if (_mouseJoint != nil){
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+        [_penguinCatapultJoint invalidate];
+        _penguinCatapultJoint = nil;
+            _currentPenguin.physicsBody.allowsRotation = TRUE;
+        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        [_contentNode runAction:follow];
+    }
+}
+
+-(void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // when touches end, meaning the user releases their finger, release the catapult
+    [self releaseCatapult];
+}
+-(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
+    [self releaseCatapult];
 }
 - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
 {
