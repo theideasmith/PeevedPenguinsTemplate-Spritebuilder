ab6e1d7e4f87c48670ab7880b4f0073bea2490d4
aclinnovator <aclscientist@gmail.com>
2014-06-24 22:16:56 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -38,41 +38,35 @@
 }
 
 // called on every touch in this scene
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    NSLog(@"touch began");
+-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
     CGPoint touchLocation = [touch locationInNode:_contentNode];
-    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation)){
-        _mouseJointNode.position = touchLocation;
-         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
-        _currentPenguin = [CCBReader load:@"Penguin"];
-        CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
-        _currentPenguin.position = [_physicsNode convertToNodeSpace:penguinPosition];
-        [_physicsNode addChild:_currentPenguin];
-        _currentPenguin.physicsBody.allowsRotation = FALSE;
-        _penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
-        
-        
-    }
     
+    // start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+    {
+        // move the mouseJointNode to the touch position
+        _mouseJointNode.position = touchLocation;
+        
+        // setup a spring joint between the mouseJointNode and the catapultArm
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+    }
 }
 
--(void)releaseCatapult {
-    if (_mouseJoint != nil){
+- (void)releaseCatapult {
+    if (_mouseJoint != nil)
+    {
+        // releases the joint and lets the catapult snap back
         [_mouseJoint invalidate];
         _mouseJoint = nil;
-        [_penguinCatapultJoint invalidate];
-        _penguinCatapultJoint = nil;
-            _currentPenguin.physicsBody.allowsRotation = TRUE;
-        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-        [_contentNode runAction:follow];
     }
 }
-
 -(void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
 {
     // when touches end, meaning the user releases their finger, release the catapult
     [self releaseCatapult];
 }
+
 -(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
 {
     // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
